535084d08235c1ad8416f4f401b54ae3ac951fa9
Yongjin Kim <span5189@naver.com>
2025-05-23 16:17:21 +0900

    첫번째 프로젝트를 기반으로 vue.js를 사용해 백과 프론트 분리

---
+++ kyj_prj/src/test/java/com/msa/kyj_prj/KyjPrjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msa.kyj_prj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KyjPrjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
