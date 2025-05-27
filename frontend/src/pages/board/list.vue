6c88c2ebfbd53f8f8122de3328cb5e278efb7865
Yongjin Kim <span5189@naver.com>
2025-05-27 16:05:33 +0900

    게시판 리스트 수정정

--- frontend/src/pages/board/list.vue
+++ frontend/src/pages/board/list.vue
@@ -1,25 +1,23 @@
 <template>
     <div class="container-fluid main-container">
 		<div class="row h-100">
-			<!-- 우측 본문 -->
-			<main class="col-md-10 main-area">
+			<main class="main-area">
 			<h2>게시물 목록</h2>
-			<!-- 건수 : <select name="size" id="size">
-				<c:forTokens items="10,30,90,100" delims="," var="size">
-					<option value="${size}"
-						${pageResponse.size == size ? 'selected' : ''}>${size}</option>
-				</c:forTokens>
-			</select> -->
+			 건수 : 
+			 <select name="size" id="size" v-model="size">
+				<option v-for="size in sizes" :key="size" :value="size">
+					{{ size }}
+				</option>
+			</select>
 
 			<!-- 현재 페이지 번호 / 전체 페이지 번호 -->
-			<!-- (${pageResponse.pageNo}/${pageResponse.totalPage}) -->
+			({{pageResponse.pageNo}}/{{pageResponse.totalPage}})
 
-			<!-- <form action="list?searchValue=${pageResponse.searchValue}"
-				name="searchID" id="searchID">
-				검색어 : <input type="text" name="searchValue" id="searchValue"
-					value="${pageResponse.searchValue}"> <input type="submit"
-					value="검색">
-			</form> -->
+			<form @submit.prevent="searchID" :name="searchID" :id="searchID">
+				검색어 : 
+				<input type="text" :name="searchValue" :id="searchValue" v-model="pageResponse.searchValue"> 
+				<input type="submit" value="검색">
+			</form>
 
 			<table class="table table-striped table-hover table-bordered mt-4">
 				<thead class="table-dark text-center">
@@ -38,7 +36,9 @@
 				<tbody class="text-center">
 						<tr v-for="item in pageResponse.list" :key="item">
 							<td>{{item.bno}}</td>
-							<td><a :href="`detailView?bno=${item.bno}`">{{item.title}}</a></td>
+							<td>
+								<router-link :to="`detailView?bno=${item.bno}`">{{item.title}}</router-link>
+							</td>
 							<td>{{item.writer}}</td>
 							<td>{{item.reg_date}}</td>
 							<td>{{item.view_count}}</td>
@@ -50,49 +50,102 @@
 				</tbody>
 			</table>
 			<!-- 페이지 처리 -->
-			<!-- <div class="d-flex justify-content-center">
-			<template v-if="pageResponse.prev">
-				<a href="list?pageNo=${pageResponse.startPage-1}&size=${pageResponse.size}
-				<c:if test='${not empty pageResponse.searchValue}'>&searchValue=${pageResponse.searchValue}
-    			</c:if>">이전 </a>
-			</template>
-			<c:forEach begin="${pageResponse.startPage}"
-				end="${pageResponse.endPage}" var="pageNo">
-				<a href="list?pageNo=${pageNo}&size=${pageResponse.size} <c:if test='${not empty pageResponse.searchValue}'>&searchValue=${pageResponse.searchValue}</c:if>">
-					<c:choose>
-						<c:when test="${pageNo == pageResponse.pageNo}">
-							<b>${pageNo}</b>
-						</c:when>
-						<c:otherwise>${pageNo}</c:otherwise>
-					</c:choose>
-				</a>&nbsp;
-			</c:forEach>
-			<c:if test="${pageResponse.next}">
-				<a
-					href="list?pageNo=${pageResponse.endPage+1}&size=${pageResponse.size}<c:if test='${not empty pageResponse.searchValue}'>&searchValue=${pageResponse.searchValue}
-    </c:if>">
-					다음 </a>
-			</c:if>
-			</div> -->
+				<div class="d-flex justify-content-center">
+					<template v-if="pageResponse.prev">
+						<router-link :to="makeUrl(pageResponse.startPage - 1)">이전 </router-link>
+					</template>
+
+					<template v-for="pageNo in pageRange" :key="pageNo">
+						<router-link :to="{name: 'Board_List', query:{pageNo, size: size,  searchValue: searchValue || undefined}}">
+							<template v-if="pageNo === pageResponse.pageNo">
+								<b>{{ pageNo }}</b>
+							</template>
+							<template v-else>
+								{{ pageNo }}
+							</template>
+						</router-link>
+						&nbsp;
+					</template>
+
+					<template v-if="pageResponse.next">
+						<router-link :to="makeUrl(pageResponse.endPage + 1)">다음</router-link>
+					</template>
+				</div>
 		</main>
 	</div>
 </div>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      pageResponse: {
-        list: [{"bno" : "1","title":"임시 제목","writer":"임시작성자","reg_date":"2025-05-26","view_count":"0"},
-          {"bno" : "2","title":"임시 제목","writer":"임시작성자","reg_date":"2025-05-26","view_count":"0"},
-          {"bno" : "3","title":"임시 제목","writer":"임시작성자","reg_date":"2025-05-26","view_count":"0"},
-          {"bno" : "4","title":"임시 제목","writer":"임시작성자","reg_date":"2025-05-26","view_count":"0"},
-          {"bno" : "5","title":"임시 제목","writer":"임시작성자","reg_date":"2025-05-26","view_count":"0"},
-          {"bno" : "6","title":"임시 제목","writer":"임시작성자","reg_date":"2025-05-26","view_count":"0"}
-        ]
-      }
-    };
-  },
-};
+
+
+<script setup>
+	import { computed, watch, reactive} from 'vue'
+	import axios from 'axios'
+	import { useRouter, useRoute } from 'vue-router'
+	const router = useRouter() // 보낼 경로
+	const route = useRoute() // 현재 경로
+	const sizes = [10, 30, 90, 100] // 건수 사이즈 
+	// 초기값 설정
+	const pageResponse = reactive ({ 
+		list: [],
+		endPage: 1,
+		next: false,
+		prev: false,
+		startPage: 1,
+		totalPage: 1
+	})
+
+	// size와 pagNo변경시 감지하여 함수 발생
+	const pageNo = computed(() => parseInt(route.query.pageNo) || 1)
+	const size = computed({
+		get: () => parseInt(route.query.size) || 10,
+		set: (val) => {
+			router.push({
+				name: 'Board_List',
+				query: {
+					...route.query,
+					pageNo: 1, // 사이즈 바꾸면 1페이지로 초기화
+					size: val
+				}
+			})
+		}
+	})
+	
+	const pageRange = computed(() => {
+	  const range = []
+	  for (let i = pageResponse.startPage; i <= pageResponse.endPage; i++) {
+		range.push(i)
+	  }
+	  return range
+	})
+	
+	watch([pageNo, size, () => route.query.searchValue], ([newPageNo, newSize, newSearch]) => {
+		fetchData(newPageNo, newSize, newSearch)
+	}, 
+	{ immediate: true }
+	)
+	
+	function searchID() {
+		router.push({
+			name: 'Board_List',
+			query: {
+			pageNo: 1, // 검색 시 1페이지로 이동
+			size: size.value,
+			searchValue: pageResponse.searchValue || null
+			}
+		})
+	}
+	// 값 변경시 다시 list 가져오도록 요청
+	function fetchData(pageNo, size, searchValue) {
+	axios.get(`/api/board/list?pageNo=${pageNo}&size=${size}&searchValue=${encodeURIComponent(searchValue || '')}`)
+		.then(res => {
+		Object.assign(pageResponse, res.data.pageResponse)
+		})
+	}
+
+	// url 변경시 반응
+	function makeUrl(pageNo) {
+	const base = `list?pageNo=${pageNo}&size=${pageResponse.size}`
+	return pageResponse.searchValue ? `${base}&searchValue=${encodeURIComponent(searchValue)}` : base
+	}
 </script>
