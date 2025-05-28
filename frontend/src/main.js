68f30c1973bc2495f9338bccf69b0e018ae60c53
Yongjin Kim <span5189@naver.com>
2025-05-28 14:25:14 +0900

    pinia추가 누락된거 커밋

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,13 @@ import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import router from './router/index.js'
 import './main.css';
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 const app = createApp(App)
+const pinia = createPinia()
+
+pinia.use(piniaPluginPersistedstate) 
+app.use(pinia);
 app.use(router);
 app.mount('#app')
